9d6581f9e44fa639c41b32df4acf7aca5ff99d09
gabriel <gabrielcmoris@gmail.com>
2025-04-04 10:46:10 +0200

    Add readiness promise input and button to PartyChoiceComponent; update translations and enhance Input component for range type

--- src/components/Promises/PartyChoiceComponent.tsx
+++ src/components/Promises/PartyChoiceComponent.tsx
@@ -1,17 +1,40 @@
 "use client";
-import React from "react";
+import React, { ChangeEvent, useCallback, useState } from "react";
 import { usePartiesContext } from "../Parties/PartyStateManager";
 import Loading from "../Loading";
 import Image from "next/image";
 import { Campaign, Party } from "@/types/politicalParties";
 import Dropdown from "../Dropdown";
+import Input from "../Input";
+import { useTranslations } from "next-intl";
+import Button from "../Button";
 
 export const PartyChoiceComponent = () => {
-  const { parties, loading, setPartyChoice, partyChoice, campaigns, setCampaignChoice, campaignChoice, structuredPromises } = usePartiesContext();
+  const [promiseReadiness, setPromiseReadiness] = useState("");
 
-  if (loading) {
-    return <Loading />;
-  }
+  const t = useTranslations("parties");
+  const {
+    parties,
+    loading,
+    setPartyChoice,
+    partyChoice,
+    campaigns,
+    setCampaignChoice,
+    campaignChoice,
+    structuredPromises,
+  } = usePartiesContext();
+
+  const onInputChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      const newValue = e.target.value;
+      setPromiseReadiness(newValue);
+    },
+    []
+  );
+
+  const sendReadyness = () => {
+    alert(promiseReadiness);
+  };
 
   const handlePartychoice = (party: Party) => {
     if (party.id === partyChoice?.id) {
@@ -21,22 +44,35 @@ export const PartyChoiceComponent = () => {
     }
   };
 
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
-    <div className="flex w-full flex-row flex-wrap gap-10 justify-center align-center">
+    <div className="flex w-full flex-col justify-center align-center">
       {!partyChoice ? (
-        parties.map((party) => {
-          return (
-            <div
-              key={party.id + "- parties"}
-              onClick={() => handlePartychoice(party)}
-              className="border cursor-pointer duration-500 bg-white flex items-center justify-center border-drPurple h-28 w-28 rounded-md hover:-translate-y-1 hover:shadow-drPurple hover:shadow-sm"
-            >
-              <Image className="w-auto" src={party.logo_url} width={100} height={100} alt={party.name + "logo"} priority />
-            </div>
-          );
-        })
+        <section className="w-full flex flex-row flex-wrap justify-center align-center gap-10 p-5 md:p-36">
+          {parties.map((party) => {
+            return (
+              <div
+                key={party.id + "- parties"}
+                onClick={() => handlePartychoice(party)}
+                className="border cursor-pointer duration-500 bg-white flex items-center justify-center border-drPurple h-28 w-28 rounded-md hover:-translate-y-1 hover:shadow-drPurple hover:shadow-sm"
+              >
+                <Image
+                  className="w-auto"
+                  src={party.logo_url}
+                  width={100}
+                  height={100}
+                  alt={party.name + "logo"}
+                  priority
+                />
+              </div>
+            );
+          })}
+        </section>
       ) : (
-        <section className="w-full flex flex-col items-center gap-10">
+        <section className="w-full flex flex-col items-center gap-10 p-5 md:px-36">
           <div
             key={partyChoice.id + "- partychoice"}
             onClick={() => handlePartychoice(partyChoice)}
@@ -44,16 +80,49 @@ export const PartyChoiceComponent = () => {
               partyChoice.id === partyChoice?.id && "border-4"
             } border cursor-pointer duration-500 bg-white flex items-center justify-center border-drPurple h-28 w-28 rounded-md hover:-translate-y-1 hover:shadow-drPurple hover:shadow-sm`}
           >
-            <Image src={partyChoice.logo_url} width={100} height={100} alt={partyChoice.name + "logo"} priority />
+            <Image
+              src={partyChoice.logo_url}
+              width={100}
+              height={100}
+              alt={partyChoice.name + "logo"}
+              priority
+            />
           </div>
-          {campaigns[0] && (
-            <Dropdown items={campaigns} choose={(item) => setCampaignChoice(item as Campaign)} choice={campaignChoice || campaigns[0]} />
-          )}
+          <div className="w-full flex flex-col gap-10 items-center justify-center">
+            {campaigns[0] && (
+              <Dropdown
+                items={campaigns}
+                choose={(item) => setCampaignChoice(item as Campaign)}
+                choice={campaignChoice || campaigns[0]}
+              />
+            )}
+            <div className="w-full flex flex-col xl:flex-row gap-5 xl:gap-20 justify-end items-end">
+              <Input
+                inputLabel={t("readiness-promise")}
+                type="range"
+                className="w-full xl:w-2/3"
+                inputString={promiseReadiness}
+                placeholder=""
+                setInput={onInputChange}
+                placeholderClass="h-12"
+              />
+              <Button
+                onClick={sendReadyness}
+                className="w-full xl:w-1/3"
+                label={t("save-promise-readiness-btn")}
+                type="submit"
+              />
+            </div>
+          </div>
+
           <section className="w-full">
             {structuredPromises.map((subject) => {
               return (
                 <div key={subject.id + "-subject"}>
-                  <div className="cursor-help text-xl font-bold font-drserif" title={subject.description}>
+                  <div
+                    className="cursor-help text-xl font-bold font-drserif"
+                    title={subject.description}
+                  >
                     {subject.name}
                   </div>
 
